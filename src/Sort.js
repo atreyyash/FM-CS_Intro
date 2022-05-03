6d7528784cb90d4c539db2a05c4d81b7dd193956
Yash Atrey <60622270+atreyyash@users.noreply.github.com>
2022-05-03 17:09:09 +0530

    sort-visualizer sort visualizer for radix sort(have some error)

--- src/Sort.js
+++ src/Sort.js
@@ -1,7 +1,61 @@
 import React from "react";
+//import { shuffle, range } from "lodash";
+import { App, snapshot, done, clear } from "./sort-visualizer";
+import "./styles.css";
 
-function Sort() {
-  return <h1>hi</h1>;
+function getDigit(number, place, longestNumber) {
+  const string = number.toString();
+  const size = string.length;
+
+  const mod = longestNumber - size;
+  return string[place - mod] || 0;
 }
 
-export default Sort;
+//longest Number
+function getLongestNumber(array) {
+  let longest = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    const currentLength = array[i].toString().length;
+    longest = currentLength > longest ? currentLength : longest;
+  }
+
+  return longest;
+}
+
+function Sort(array) {
+  //find Longest Number
+  snapshot(array);
+  const longestNumber = getLongestNumber(array);
+
+  //create how many bucket you need
+  //an array of 10
+  const buckets = new Array(10).fill().map(() => []);
+
+  for (let i = longestNumber; i >= 0; i--) {
+    while (array.length) {
+      const current = array.shift();
+      buckets[getDigit(current, i, longestNumber)].push(current);
+    }
+
+    for (let j = 0; j < buckets.length; j++) {
+      while (buckets[j].length) {
+        array.push(buckets[j].shift());
+      }
+    }
+    snapshot(array);
+  }
+
+  return array;
+}
+
+export default function SortComponent() {
+  clear();
+  const fill = 99;
+  const nums = new Array(fill)
+    .fill()
+    .map(() => Math.floor(Math.random() * 500000));
+  Sort(nums);
+  done();
+  return <App />;
+}
